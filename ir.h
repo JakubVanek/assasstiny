13c1f830652c05efe585f78d880611b77558c7a7
Jakub Vaněk <vanek.jakub4@seznam.cz>
2017-06-01 13:49:08 +0200

    Add IR double buffer

--- ir.h
+++ ir.h
@@ -4,13 +4,21 @@
 #include "config.h"
 
 typedef struct {
-    ir_sample_t buffer[IR_FIFO_SIZE];
-    ir_sample_t current_ticks;
-    ir_size_t   buffer_size;
-    bool        current_state;
-    bool        notify_state;
+    ir_sample_t data[IR_FIFO_SIZE];
+    ir_size_t   length;
+} ir_buffer_t;
+
+typedef struct {
+    ir_buffer_t volatile *current_buffer;
+    ir_sample_t           current_ticks;
+    ir_size_t             buffer_size;
+    bool                  current_state;
+    bool                  notify_main;
+    bool                  current_processed;
 } irdata_t;
 
 extern volatile irdata_t irdata;
+
+void initIR();
 
 #endif
